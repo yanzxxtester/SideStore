c2e89b09ead25413fb242d376030f8767ccf058c
naturecodevoid <44983869+naturecodevoid@users.noreply.github.com>
2023-02-13 21:44:48 -0800

    RootView: Fix UI being shoved into sidebar on iPad (closes #264, thanks @Swifticul!)

--- AltStore/Views/RootView.swift
+++ AltStore/Views/RootView.swift
@@ -19,6 +19,7 @@ struct RootView: View {
                 NavigationView {
                     content(for: tab)
                 }
+                .navigationViewStyle(StackNavigationViewStyle())
                 .tag(tab)
                 .tabItem {
                     tab.label
